e72ce83bbcc0fa6777f26ba40e745d6cde08db24
Jayakar,Avinash <avithemad@gmail.com>
2024-01-01 03:49:09 +0530

    Added parsing for term expression, except operational and ifthenelse

--- parser.cpp
+++ parser.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<string>
 
+#define NEWNODE (struct ast_node*)malloc(sizeof(struct ast_node));
 
 struct ast_node {
 	int node_type;
@@ -13,6 +14,8 @@ struct ast_node {
 };
 
 enum Token {
+	tok_comma=-11,
+	tok_apply=-10,
 	tok_variable=-9,
 	tok_lamda=-8,
 	tok_boolean=-7,
@@ -40,19 +43,34 @@ enum AstNodeType {
 	node_variable,
 	node_value,
 	node_integer,
-	node_boolean
+	node_boolean,
+	node_app
 };
 
-struct ast_node* parse_value() {
+struct ast_node* parse_value();
+struct ast_node* parse_term();
+struct ast_node* parse_variable() {
 	int current_index = parsing_idx;
-	
-	if (token_stream[parsing_idx++] == tok_lamda) {
+
+	if (token_stream[parsing_idx++] != tok_variable) {
+		// Handle error
+		return NULL;
+	};
+
+	struct ast_node* var_node = (struct ast_node*)malloc(sizeof(struct ast_node));
+	var_node->node_type = node_variable;
+	var_node->identifier = id_map[current_index];
+
+	return var_node;
+}
+
+struct ast_node* parse_lamda() {
+		if (token_stream[parsing_idx++] != tok_lamda) return NULL;
 		if (token_stream[parsing_idx++] != tok_l_paren) {
 			return NULL;
 		}
-		int var_idx;
-		var_idx = parsing_idx;
-		if (token_stream[parsing_idx++] != tok_variable) {
+		struct ast_node* leaf_var_node = parse_variable();
+		if (leaf_var_node == NULL) {
 			return NULL;
 		}
 		if (token_stream[parsing_idx++] != tok_r_paren) {
@@ -61,30 +79,23 @@ struct ast_node* parse_value() {
 		if (token_stream[parsing_idx++] != tok_l_curl) {
 			return NULL;
 		}
-		struct ast_node* inner_value = parse_value();
+		struct ast_node* inner_value = parse_term();
 		if (inner_value == NULL) return NULL;
 		
 		if (token_stream[parsing_idx++] != tok_r_curl) {
 			return NULL;
 		}
 
-		struct ast_node* value_node = (struct ast_node*)malloc(sizeof(struct ast_node));
-		struct ast_node* leaf_lamda_node = (struct ast_node*)malloc(sizeof(struct ast_node));
-		leaf_lamda_node->node_type = node_lamda;
+		struct ast_node* lamda_node = (struct ast_node*)malloc(sizeof(struct ast_node));
+		lamda_node->node_type = node_lamda;
 
-		struct ast_node* leaf_var_node = (struct ast_node*)malloc(sizeof(struct ast_node));
-		leaf_var_node->node_type = node_variable;
-		leaf_var_node->identifier = id_map[var_idx];
+		lamda_node->children[0] = leaf_var_node;
+		lamda_node->children[1] = inner_value;
 
-		value_node->node_type = node_value;
-		value_node->children[0] = leaf_lamda_node;
-		value_node->children[1] = leaf_var_node;
-		value_node->children[2] = inner_value;
+		return lamda_node;
+}
 
-		return value_node;
-	}
-
-	parsing_idx = current_index;
+struct ast_node* parse_integer() {
 	if (token_stream[parsing_idx++] == tok_integer) {
 		struct ast_node* int_node = (struct ast_node*)malloc(sizeof(struct ast_node));
 		int_node->node_type = node_integer;
@@ -95,6 +106,87 @@ struct ast_node* parse_value() {
 
 	return NULL;
 }
+
+struct ast_node* parse_value() {
+	int current_index = parsing_idx;
+	
+	if (token_stream[current_index] == tok_lamda) {
+		struct ast_node* lamda_node = parse_lamda();
+		if (lamda_node == NULL) return NULL;
+
+		return lamda_node;
+	}
+
+	if (token_stream[current_index] == tok_integer) {
+		struct ast_node* int_node = parse_integer();
+		if (int_node == NULL) return NULL;
+
+		return int_node;
+	}
+
+	return NULL;
+}
+
+struct ast_node* parse_app() {
+	if (parsing_idx >= token_stream.size()) return NULL;
+	int current_index = parsing_idx;
+
+	if (token_stream[parsing_idx++]!=tok_apply) return NULL;
+	if (token_stream[parsing_idx++]!=tok_l_paren) return NULL;
+	struct ast_node* t1 = parse_term();
+
+	if (token_stream[parsing_idx++]!=tok_comma) {
+		return NULL;
+	}
+	struct ast_node* t2 = parse_term();
+	if (token_stream[parsing_idx++]!=tok_r_paren) {
+		return NULL;
+	}
+	struct ast_node* app_node = NEWNODE;
+	app_node->node_type = node_app;
+	app_node->children[0] = t1;
+	app_node->children[1] = t2;
+	return app_node;
+}
+
+struct ast_node* parse_boolean() {
+	if (token_stream[parsing_idx++] == tok_boolean) {
+		struct ast_node* bool_node = (struct ast_node*)malloc(sizeof(struct ast_node));
+		bool_node->node_type = node_boolean;
+		bool_node->identifier = id_map[parsing_idx-1];
+
+		return bool_node;
+	}
+
+	return NULL;
+}
+
+struct ast_node* parse_term() {
+	int current_index = parsing_idx;
+
+	if (token_stream[current_index] == tok_variable) {
+		// if you see a variable, either grammer can happen
+		// t op t, just a variable
+		return parse_variable();
+	}
+	if (token_stream[current_index] == tok_lamda) {
+		return parse_lamda();
+	}
+	if (token_stream[current_index] == tok_apply) {
+		return parse_app();
+	}
+	if (token_stream[current_index] == tok_integer) {
+		return parse_integer();
+	}
+	if (token_stream[current_index] == tok_boolean) {
+		return parse_boolean();
+	}
+	// TODO: parse operand and ifthenelse...
+
+	return NULL;
+}
+
+
 
 // Lexer Implementation
 
@@ -113,7 +205,10 @@ bool is_num(int ch) {
 int get_token() {
 	static int last_char = ' ';
 	while(is_space(last_char)) last_char = getchar();
-
+	if (last_char == ',') {
+		last_char = getchar();
+		return tok_comma;
+	}
 	if (is_alpha(last_char)) {
 		Identifier = last_char;
 		while (is_alpha(last_char = getchar())) {
@@ -126,6 +221,10 @@ int get_token() {
 
 		if (Identifier == "true" || Identifier == "false") {
 			return tok_boolean;
+		}
+
+		if (Identifier == "apply") {
+			return tok_apply;
 		}
 		return tok_variable;
 	}
@@ -168,14 +267,16 @@ int get_token() {
 }
 
 void dfs(struct ast_node* node) {
-	if (node->node_type == node_value) {
-		std::cout << "Value node";
-	} else if (node->node_type == node_variable) {
+	if (node->node_type == node_variable) {
 		std::cout << "Variable node : " << node->identifier;
 	} else if (node->node_type == node_lamda) {
-		std::cout << "Lamda node : " << node->identifier;
+		std::cout << "Lamda node";
 	} else if (node->node_type == node_integer) {
 		std::cout << "Integer node : " << node->value;	
+	} else if (node->node_type == node_app) {
+		std::cout << "Apply node";
+	} else if (node->node_type == node_boolean) {
+		std::cout << "Boolean node: " << node->identifier;
 	}
 	std::cout << std::endl;
 	for (int i=0; i<3; i++) {
@@ -192,7 +293,9 @@ int main() {
 	int i=0;
 	 
 
-	struct ast_node* ast = parse_value();
+	struct ast_node* ast;
+	int si = parsing_idx;
+	ast = parse_term();
 	if (ast!= NULL ) {
 		dfs(ast);
 	} else {
